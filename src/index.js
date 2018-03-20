3171f6e3f32df441c93dc5c65627ce11be4d54d5
Josh David Miller <josh@joshdmiller.com>
2018-03-20 05:01:27 +0000

    feat: create rest api framework

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+
+import v1 from './v1';
+
+const app = express();
+
+app.use( cors() );
+
+/**
+ * FIXME: placeholder until there is user auth
+ */
+app.use( ( req, res, next ) => {
+  req.user = {
+    id: '000',
+    email: 'josh@joshdmiller.com',
+    name: 'Josh David Miller',
+  };
+
+  next();
+});
+
+app.use( '/v1', v1 );
+
+app.listen( 8080, () => console.log( 'Server is listening on port 8080...' ) );
+
---
+++ src/v1/index.js
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+
+const api = Router();
+
+api.get( '/users/:id', ( req, res ) => {
+  if ( req.params.id === req.user.id ) {
+    return res.send( req.user );
+  }
+
+  res.status( 404 ).send({ message: 'user not found' });
+});
+
+export default api;
+
